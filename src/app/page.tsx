786c000015e9b855452ff12327b4924a34e28cb1
Zaman Ali <zamanyaseen2024@gmail.com>
2025-10-03 18:28:17 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,25 @@ import React from "react";
 import Cookies from "js-cookie";
 import Introduction from "@/components/Introduction";
 // import AddWebsite from "./pages/add-website/page";
-import Dashboard from "./user-dashboard/page";
+// import Dashboard from "./user-dashboard/page";
 // import CenteredCards from "./pages/main/page";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { Loader2 } from "lucide-react";
+
+const Dashboard = dynamic(() => import("./user-dashboard/page"));
 
 export default function Home() {
   const isLoggedIn = Boolean(Cookies.get("token"));
-  return <div>{isLoggedIn ? <Dashboard /> : <Introduction />}</div>;
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Suspense fallback={<Loader2 className="animate-spin" size={40} />}>
+          <Dashboard />
+        </Suspense>
+      ) : (
+        <Introduction />
+      )}
+    </div>
+  );
 }
